ce017768e83a00395fb2909af79e1d5a44799cc1
MalvikaShetty <shettymalvika11@gmail.com>
2021-09-14 12:53:17 +0530

    linked pages

--- gwp/gwapp/urls.py
+++ gwp/gwapp/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.logindash, name='loginDash'),
+    path('', views.home, name='Home'),
     path('maps/', views.maps, name='Maps'),
     path('add_data/', views.add_data, name='addData'),
+    path('dashboard/', views.dashboard, name='Dashboard'),
+    path('own_data/', views.own_data, name='ownData'),
+    path('profile/', views.profile, name='Profile'),
+    path('reports/', views.reports, name='Reports'),
+    path('view_data/', views.view_data, name='viewData'),
+    path('login/', views.login, name='Login'),
+    path('signup/', views.signup, name='SignUp'),
+    path('start/', views.start_popup, name='Start'),
 ]
--- gwp/gwapp/views.py
+++ gwp/gwapp/views.py
@@ -4,8 +4,8 @@ from django.http import (HttpResponse, HttpResponseNotFound, Http404,
 # Create your views here.
 
 
-def logindash(request):
-    return render(request, 'base.html')
+def home(request):
+    return render(request, 'home.html')
 
 
 def maps(request):
@@ -14,3 +14,35 @@ def maps(request):
 
 def add_data(request):
     return render(request, 'add_data.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+def own_data(request):
+    return render(request, 'own_data.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def reports(request):
+    return render(request, 'reports.html')
+
+
+def view_data(request):
+    return render(request, 'view_data.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def start_popup(request):
+    return render(request, 'start.html')
